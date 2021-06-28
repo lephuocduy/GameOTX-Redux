1d3fb859502312f9ed99b57a607ac535f5f39fdc
lephuocduy <duylp.work@gmail.com>
2021-06-29 00:29:38 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BaiTapGameOTX from './BaiTapGameOTX/BaiTapGameOTX';
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapGameOTX />
+    </div>
+  );
+}
+
+export default App;
